47887c6e66c100076c3be6cc38ced14fd3e06f1b
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-08-14 10:32:48 +0530

    deleted the client

--- client/src/Pages/User/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
-const PrivateRoutes = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoutes;
