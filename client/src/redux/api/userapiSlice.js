57ae3df6d329b0e25f18e690206df4f7250304b0
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-08-18 02:30:58 +0530

    added user api && store

---
+++ client/src/redux/api/userapiSlice.js
@@ -0,0 +1,43 @@
+import { apiSlice } from "./apiSlice";
+import { USERS_URL } from "../constants";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    getUserProfile: builder.query({}),
+    updateUserProfile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useLogoutMutation,
+  useGetUserProfileQuery,
+  useUpdateUserProfileMutation,
+} = userApiSlice;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
+import authReducer from "./features/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
-// setupListeners();
+setupListeners(store.dispatch);
 
 export default store;
